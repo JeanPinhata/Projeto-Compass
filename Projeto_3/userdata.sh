f8bcf4fbb7657d933bfb1b204525f7c79f0f9291
JeanPinhata <jeanpinhata@gmail.com>
2025-04-03 15:53:35 -0300

    Adicionado novo projeto Projeto_3

---
+++ Projeto_3/userdata.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Atualiza os pacotes do sistema
+yum update -y
+
+# Instala pacotes necessários
+yum install -y ca-certificates wget amazon-efs-utils
+
+# Instala o Docker
+yum install -y docker
+
+# Inicia e habilita o serviço do Docker
+systemctl enable docker
+systemctl start docker
+
+# Adiciona o usuário ec2-user ao grupo docker para evitar uso de sudo
+usermod -aG docker ec2 -user
+
+# Instala o Docker Compose manualmente
+curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+# Cria o diretório de montagem do EFS
+mkdir -p /data
+
+# Monta o sistema de arquivos EFS
+# Substituir pelo mount do seu EFS
+sudo mount -t efs -o tls fs-04b1f35e5f4903d02:/ efs
+
+#Pega o docker compose do github
+wget -O /home/ec2-user/docker-compose.yml https://raw.githubusercontent.com/JeanPinhata/Projeto-Compass/refs/heads/main/Projeto_3/docker-compose.yml
+sudo chown ec2-user:ec2-user /home/ec2-user/docker-compose.yml
+
+# Ajusta permissões do arquivo
+chown ec2-user:ec2-user /home/ec2-user/docker-compose.yml
+
+# Inicia os containers com Docker Compose
+cd /home/ec2-user && docker-compose up -d
+
+# Ajusta permissões do arquivo
+chown ec2-user:ec2-user /home/ec2-user/docker-compose.yml
+
+# Inicia os containers com Docker Compose
+cd /home/ec2-user && docker-compose up -d
