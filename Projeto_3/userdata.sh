4d909b26d86a39b5159ab8580650c6dc8510d4b5
Jean Pinhata <142039051+JeanPinhata@users.noreply.github.com>
2025-04-07 12:15:47 -0300

    Update userdata.sh

--- Projeto_3/userdata.sh
+++ Projeto_3/userdata.sh
@@ -1,40 +1,26 @@
 #!/bin/bash
 
-# Atualiza os pacotes do sistema
-yum update -y
+sudo yum update -y
+sudo yum install -y docker wget amazon-efs-utils
 
-# Instala pacotes necessários
-yum install -y ca-certificates wget amazon-efs-utils
+sudo service docker start
+sudo systemctl enable docker.service
+sudo usermod -aG docker ec2-user
 
-# Instala o Docker
-yum install -y docker
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
-# Inicia e habilita o serviço do Docker
-systemctl enable docker
-systemctl start docker
+sudo mkdir -p /wordpress
+Colocar aqui o mount do EFS
 
-# Adiciona o usuário ec2-user ao grupo docker para evitar uso de sudo
-usermod -aG docker ec2 -user
+if mountpoint -q /wordpress; then
+    echo "EFS montado com sucesso em /wordpress"
+else
+    echo "Falha ao montar EFS"
+fi
 
-# Instala o Docker Compose manualmente
-curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-
-# Cria o diretório de montagem do EFS
-mkdir -p /data
-
-# Monta o sistema de arquivos EFS
-# Substituir pelo mount do seu EFS
-sudo mkdir /efs
-sudo mount -t efs -o tls fs-04b1f35e5f4903d02:/ /efs
-
-#Pega o docker compose do github
 wget -O /home/ec2-user/docker-compose.yml https://raw.githubusercontent.com/JeanPinhata/Projeto-Compass/refs/heads/main/Projeto_3/docker-compose.yml
 sudo chown ec2-user:ec2-user /home/ec2-user/docker-compose.yml
 
-# Ajusta permissões do arquivo
-chown ec2-user:ec2-user /home/ec2-user/docker-compose.yml
-
-# Inicia os containers com Docker Compose
-cd /home/ec2-user && docker-compose up -d
+cd /home/ec2-user
+sudo docker-compose up -d
